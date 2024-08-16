3bb053904bc79c7d3a2f3b35160b40ec25632ab3
2mmanu <emmanuele.lacavalla@gmail.com>
2024-08-16 16:21:06 -0700

    fix

--- agentbuddy/agent/app.py
+++ agentbuddy/agent/app.py
@@ -1,5 +1,7 @@
-from .api_v1 import app, port
+import os
 import uvicorn
+from agentbuddy.agent.api_v1 import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(port), workers=1)
+    port = int(os.getenv("AGENT_PORT", default="8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port, workers=1)
